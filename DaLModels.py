aff9a5e5d3ca9dda6e7d342324ba28fd7ccbc764
Anony0110 <zz.xiang.work@gmail.com>
2025-03-15 16:50:16 +0800

    Add files via upload

--- DaLModels.py
+++ DaLModels.py
@@ -4,10 +4,7 @@ import math
 from scipy import stats
 import numpy as np
 import pandas as pd
-from river.base import DriftDetector, drift_detector
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
-from sklearn.feature_selection import mutual_info_regression
-from sklearn.model_selection import GridSearchCV
 from sklearn.tree import DecisionTreeRegressor
 from imblearn.over_sampling import SMOTE
 from sklearn.metrics import mean_absolute_percentage_error
@@ -63,11 +60,6 @@ def get_tree_info(tree, d):
 
 
 def info_compare(old, new):
-    """
-    :param old:
-    :param new:
-    :return: if old and new are the same
-    """
     result = [old[0].__eq__(new[0]), old[1].__eq__(new[1])]
     return result  # todo:bug
 
@@ -98,54 +90,25 @@ def compute_hoeffding_bound(G_best, G_second, range_val, confidence, n):
     return G_best - G_second > hoeffding_bound
 
 
-"""
-1. 数据不足等待数据，足则直接训练，在检测到下次drift之前保持静止 -> 
-2. 立即训练，随着到达的新数据增量训练 MLP -> 
-3. 立即训练，随着新数据累计到达重新训练 MLP n ->  
-4. 保存两个模型，长期模型和当前模型 
-"""
-
-"""
-第一级是一个current data的总集合
-第二级是各个cluster子集
-1-》2: clusters change使得第二级变化
-2-》1: local current data收缩导致第一级变化
-第一级没有主动的遗忘策略，依赖第二级遗忘
-数据必须遗忘，否则总体数据无法反应当前的数据
-模型可以保留，作为老数据（老概念）的抽象记忆
-"""
-baseline_models = ['LR', 'HT', 'KNN', 'RF', 'DeepPerf', 'ARF', 'SRP', 'SVR', 'KRR']
-
-
+baseline_models = ['LR', 'HT', 'KNN', 'RF']
 class DaL_Regressor:
-    def __init__(self, depth, min_clock=32, MODEL_REUSE=True, base_model="RF", Alignment_frequency=1,
-                 HYBRID_UPDATE=True, Upper_Adapt=True, Lower_Adapt=True):
+    def __init__(self, depth=1, min_clock=32, base_model="RF", Hybrid_Frequency=3,
+                 Hybrid_Update=True, Upper_Adapt=True, Lower_Adapt=True):
         self.base_model = base_model
         self.test_mode = True
-        self.MODEL_REUSE = MODEL_REUSE
-        """self.CART_FILTER = CART_FILTER
-        self.CART_CHANGE = CART_CHANGE"""
+        self.MODEL_REUSE = True
         self.Upper_Adapt = Upper_Adapt
         self.Lower_Adapt = Lower_Adapt
-        self.HYBRID_UPDATE = HYBRID_UPDATE
-        # self.CHANGE_FILTER = CHANGE_FILTER
-        self.stable_represent = False
+        self.HYBRID_UPDATE = Hybrid_Update
         self.init = True
         self.models = list()
-        # self.detectors = list()
-        self.CART = None
-        self.rfc = None
+        self.CART = None  # to extract the dividing conditions
+        self.rfc = None  # to classify streaming samples
         self.clusters_index = list()
-
-        self.config = dict()
-        self.lr = 0.001
         # self.clusters = list()
-        self.weights = None
         self.depth = depth
         self.clusters_num = 0
         self.feature_count = 0
-        self.max_X = 0
-        self.max_Y = 0
         self.current_data_index = None
         self.whole_data = None
         self.clock = 0
@@ -156,23 +119,16 @@ class DaL_Regressor:
         self.recent_error = list()
         self.mean_error = list()
         self.count_test = list()
-        self.last_data_index = list()  # record the tail item of the last change window
         self.val_list = list()
-
         self.warning_detectors = []
         self.drift_detectors = []
         self.modes = []
         self.data_size = []
         self.model_manager = ModelsManager()
-        self.retrain_clock = Alignment_frequency * min_clock
-        self.myt = 0
-
-    def get_weight(self, x, y):
-        feature_weights = mutual_info_regression(x, y, random_state=0)
-        self.weights = feature_weights
-        return feature_weights.tolist()
+        self.retrain_clock = Hybrid_Frequency * min_clock
 
     def divide_cluster(self, x, y):
+        # current data -> divisions
         DT = DecisionTreeRegressor(random_state=3, criterion='squared_error', splitter='best')
         DT.fit(x, y)
         self.CART = DT
@@ -182,64 +138,8 @@ class DaL_Regressor:
                                                  0, cluster_indexes_all)
         return cluster_indexes_all
 
-    def label_clusters(self):
-        clusters_list = list()
-        for i in range(len(self.clusters_index)):
-            clusters_list.append(np.full(len(self.clusters_index[i]), i))
-        return np.concatenate(clusters_list)
-
-    # note: now we delete the preprocessing method for following reasons:
-    # 1. max value change lead to bigger error or more retrain
-    # 2. hard to adapt absolute index
-    def DNNs_data_preprocessing_online(self, x, y):
-        x_train = list()
-        y_train = list()
-        for i in range(len(self.clusters_index)):  # for each cluster
-            temp_X = x[self.clusters_index[i], 0:self.feature_count]
-            temp_Y = y[self.clusters_index[i]]
-            # Scale X and Y
-            x_train.append(np.divide(temp_X, self.max_X))
-            y_train.append(np.divide(temp_Y, self.max_Y))
-        return x_train, y_train
-
-    def DNNs_data_preprocessing(self, x, y):
-        x_train = list()
-        y_train = list()
-        max_X = np.amax(x, axis=0)  # scale X to 0-1
-        if 0 in max_X:
-            max_X[max_X == 0] = 1
-        max_Y = np.max(y) / 100  # scale Y to 0-100
-        if max_Y == 0:
-            max_Y = 1
-        self.max_Y = max_Y
-        self.max_X = max_X
-        for i in range(len(self.clusters_index)):  # for each cluster
-            temp_X = x[self.clusters_index[i], 0:self.feature_count]
-            temp_Y = y[self.clusters_index[i]]
-            # Scale X and Y
-            x_train.append(np.divide(temp_X, max_X))
-            y_train.append(np.divide(temp_Y, max_Y))
-        return x_train, y_train
-
-    def single_DNN_data_preprocessing(self, x, y):
-        X_train = np.divide(x, self.max_X)
-        y_train = np.divide(y, self.max_Y)
-        return X_train, y_train
-
-    def predict_data_preprocessing(self, x):
-        # 是否有必要更新
-        x_test = np.divide(x, self.max_X)
-        return x_test
-
-    def fit_RFRs(self, clustersx, clustersy):
-        self.models = list()
-        for i in range(len(self.clusters_index)):
-            # print('Training DNN for division {}... ({} samples)'.format(i + 1, len(clustersx[i])))
-            model = build_incremental_model(self.base_model)
-            model.fit(clustersx[i], clustersy[i])
-            self.models.append(model)
-
     def fit_basemodels(self, clustersx, clustersy):
+        # train local model for each division
         self.models = list()
         for i in range(len(self.clusters_index)):
             # print('Training DNN for division {}... ({} samples)'.format(i + 1, len(clustersx[i])))
@@ -248,6 +148,7 @@ class DaL_Regressor:
             self.models.append(model)
 
     def init_detectors(self):
+        # warning - drift detector for each division
         for i in range(len(self.models)):
             """detector = drift_detection.ADWIN(delta=configs.DELTA)
             self.detectors.append(detector)"""
@@ -265,7 +166,8 @@ class DaL_Regressor:
         self.rfc.fit(x, y)
         return
 
-    def fit_model(self, x, y):  # remove smote and weight
+    def fit_model(self, x, y):
+        # training DHDA
         self.whole_data = combine_x_y(x, y)
         self.current_data_index = list(range(len(self.whole_data)))
         self.clock = len(y)
@@ -315,11 +217,8 @@ class DaL_Regressor:
             self.count_test.append(0)
 
     def predict(self, x):
-        # x_test = self.predict_data_preprocessing(x)
-        # get the cluster index of test data
-        # weighted_x = np.array(x_test) * self.weights
+        # get configurations' label and predict
         test_labels = self.rfc.predict(x)
-        # 以下是按照x顺序进行predict
         pre_y = []
         for i, label in enumerate(test_labels):
             # print(f"pre in cluster{test_labels}")
@@ -327,13 +226,11 @@ class DaL_Regressor:
             pre_y.append(y[0])
         return pre_y
 
-    def learn_data(self, x, y, val):  #维护 whole_data current_data clusters_index
+    def learn_data(self, x, y, val):
         # learn when new data are available
         new_data = combine_x_y(x, y)
         self.whole_data = combine_data(self.whole_data, new_data)
         self.val_list.append(val)
-        self.myt += 1
-
 
         for i, data in enumerate(x):
             self.clock += 1
@@ -354,7 +251,7 @@ class DaL_Regressor:
                 for times in range(weights):
                     self.warning_detectors[cluster_index].add_element(sq_val)
                     if self.warning_detectors[cluster_index].detected_change():
-                        print(f"warning happen in index{cluster_index} at index{self.clock} at{self.clock / 32}")
+                        # print(f"warning happen in index{cluster_index} at index{self.clock} at{self.clock / 32}")
                         self.modes[cluster_index] = True
                         self.data_size[cluster_index] = 0
                         self.warning_detectors[cluster_index].reset()
@@ -362,9 +259,10 @@ class DaL_Regressor:
                     self.drift_detectors[cluster_index].add_element(sq_val)
 
                     if self.drift_detectors[cluster_index].detected_change():
+                        # Drift detected in the cluster_indexth division
                         mean_recent = np.mean(self.recent_error[cluster_index])
                         mean_old = self.mean_error[cluster_index]
-                        if mean_recent < mean_old:  # 向下漂移 忽略
+                        if mean_recent < mean_old:  # downward drift
                             self.modes[cluster_index] = False
                             self.drift_detectors[cluster_index].reset()
                             self.warning_detectors[cluster_index].reset()
@@ -374,7 +272,7 @@ class DaL_Regressor:
                                 self.warning_detectors[cluster_index].add_element(val)
                                 self.drift_detectors[cluster_index].add_element(val)
                             print(
-                                f"drift happen in index{cluster_index} at index{self.clock} at{self.clock / 32} but ignore")
+                                f"downward drift happen in index{cluster_index} at index{self.clock} at{self.clock / 32}")
                         else:
                             print(f"drift happen in index{cluster_index} at index{self.clock} at{self.clock / 32}")
                             self.modes[cluster_index] = False
@@ -450,17 +348,17 @@ class DaL_Regressor:
                             # model = self.model_manager.get_model_by_accuracy_threshold(train_x, train_y, cluster_index)
                             print(f"warning window size: {len(train_y)}")
                             # self.retrain_model_rfr(cluster_index, train_x, train_y)  # change
-            else:  #use most resent 128 sample
+            else:  # if lower adaption is not allowed, use most resent 256 sample to train local model
                 self.current_data_index = self.current_data_index[-256:]
 
             if self.clock % self.min_clock == 0:
+                # updated in batches
                 current_x, current_y = get_attributes_result(self.whole_data[self.current_data_index])
                 current_DT = DecisionTreeRegressor(random_state=3, criterion='squared_error', splitter='best')
                 current_DT.fit(current_x, current_y)
                 current_tree_info = get_tree_info(current_DT.tree_, 1)
                 old_tree_info = get_tree_info(self.CART.tree_, 1)
 
-                # 如果当前误差不够小 or 数据少->需要学习新数据
                 if np.mean(self.recent_error[cluster_index]) >= self.learn_threshold or len(
                         self.clusters_index[cluster_index]) < 128:
                     if self.HYBRID_UPDATE:
@@ -501,9 +399,9 @@ class DaL_Regressor:
                                 new_y = train_y[index][-self.min_clock:]
                                 self.models[index].update(new_x, new_y)
 
-                # whether cart change happens
                 cart_result = info_compare(old_tree_info, current_tree_info)
                 if not (cart_result[0] and cart_result[1]):
+                    # Upper change
                     isChange = True
                     old_best_feature = self.CART.tree_.feature[0]
                     new_best_feature = current_DT.tree_.feature[0]
@@ -535,34 +433,12 @@ class DaL_Regressor:
                                                             max_depth=self.depth, min_samples=0,
                                                             cluster_indexes_all=clusters_index)
 
-
                         self.clusters_index = clusters_index
                         print("the new clusters info:")
                         for index in range(len(self.clusters_index)):
                             print(
                                 f"the {index}-th cluster contain samples count {len(self.clusters_index[index])}, from {self.clusters_index[index][0]} to {self.clusters_index[index][-1]}")
-                        """for index, num in enumerate(similar_list):
-                            print(f"the {index}-th old&new clusters has {num} percent of the same data.")"""
                         y_label = list()
-
-                        # in order to keep window size and data consistent that may be breached when cart change
-                        """for index, d in enumerate(self.detectors):
-                            d.reset()
-                            for data_index in self.clusters_index[index]:
-                                d.add_element(self.val_list[data_index])
-                            window_size = d.width
-                            delete_size = len(self.clusters_index[index]) - window_size
-                            delete_data = self.clusters_index[index][:delete_size]
-                            self.clusters_index[index] = self.clusters_index[index][-window_size:]
-
-                            for item in delete_data:
-                                while item in self.current_data_index:
-                                    self.current_data_index.remove(item)"""
-
-                        """for index, d in enumerate(self.warning_detectors):
-                            d.reset()
-                        for index, d in enumerate(self.drift_detectors):
-                            d.reset()"""
                         # label attrs with cluster index
                         for index in self.current_data_index:
                             for label, cluster in enumerate(self.clusters_index):
@@ -571,8 +447,6 @@ class DaL_Regressor:
                                     continue
                         self.fit_rfc(current_x, y_label)
 
-                        # dnn_x, dnn_y = self.DNNs_data_preprocessing_online(current_x, current_y)
-                        # self.fit_DNNs(dnn_x, dnn_y) change
                         train_x = list()
                         train_y = list()
                         self.model_manager.change_to_cart(current_tree_info)
@@ -582,13 +456,10 @@ class DaL_Regressor:
                             x_temp, y_temp = get_attributes_result(train_data_temp)
                             train_x.append(x_temp)
                             train_y.append(y_temp)
-                        # self.fit_RFRs(train_x, train_y)
-                        # 尝试在cart对应pool中搜寻合适的模型
-                        # 若没搜寻到则重新训练
                         if self.MODEL_REUSE:
                             for m in range(len(self.models)):
                                 model = self.model_manager.get_model_by_accuracy(train_x[m], train_y[m], m)
-                                if model is None :#or len(train_y[m] > 64)
+                                if model is None:  #or len(train_y[m] > 64)
                                     if self.base_model in baseline_models:
                                         self.models[m].fit(train_x[m], train_y[m])
                                 else:
@@ -646,15 +517,7 @@ class ModelsManager:
 
     def add_model_in_current_cart(self, model, index):
         self.models[self.current_cart_index][index].append(model)
-        """if self.has_similar_model(model):
-            # todo: replace it or discard
-            pass
-        else:
-            # todo: add it in history pool
-            self.models[self.current_cart_index][index].append(model)"""
 
-    def has_similar_model(self, model):
-        pass
 
     def get_model_by_accuracy(self, x, y, cluster_index):
         if len(self.models[self.current_cart_index][cluster_index]) == 0:
